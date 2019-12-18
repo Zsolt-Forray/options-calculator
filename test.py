ba90dd809f307508d70fa46098e71a70305bb072
Zsolt Forray <forray.zsolt@gmail.com>
2019-12-18 10:42:46 +0100

    Add test functions

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ Test for the options calculator
 __author__  = 'Zsolt Forray'
 __license__ = 'MIT'
 __version__ = '0.0.1'
-__date__    = '26/11/2019'
+__date__    = '18/12/2019'
 __status__  = 'Development'
 
 
@@ -23,80 +23,94 @@ class TestOptionPrice(unittest.TestCase):
         data = (65.0, 60.0, 30.0, 35.0, 2.493)
         price = bs.OptionPrice(*data)
         result = price.call_price()
-        self.assertAlmostEqual(first=5.8523, second=result, places=2)
+        self.assertEqual(5.85, result)
 
     def test_put_price(self):
-        data = (65.0, 60.0, 30.0, 35.0, 2.493)
+        data = (62.0, 60.0, 30.0, 25.0, 1.62)
         price = bs.OptionPrice(*data)
         result = price.put_price()
-        self.assertAlmostEqual(first=0.7279, second=result, places=2)
+        self.assertEqual(0.9, result)
 
     def test_invalid_param(self):
         data = (65.0, 60.0, 30.0, 0.0, 2.493)
         with self.assertRaises(InvalidDataError):
             bs.OptionPrice(*data)
 
+    def test_missing_param(self):
+        data = (65.0, 60.0, 30.0, 25.0)
+        with self.assertRaises(TypeError):
+            bs.OptionPrice(*data)
 
 class TestOptionGreeks(unittest.TestCase):
     def test_call_delta(self):
         data = (150.0, 180.0, 350.0, 20.0, 2.493)
         greeks = bs.OptionGreeks(*data)
         result = greeks.call_delta()
-        self.assertAlmostEqual(first=0.2386, second=result, delta=0.0002)
+        self.assertEqual(0.2386, result)
 
     def test_put_delta(self):
         data = (20.0, 20.0, 50.0, 70.0, 2.493)
         greeks = bs.OptionGreeks(*data)
         result = greeks.put_delta()
-        self.assertAlmostEqual(first=-0.4432, second=result, delta=0.0002)
+        self.assertEqual(-0.4433, result)
 
     def test_call_gamma(self):
         data = (15.0, 30.0, 35.0, 100.0, 2.493)
         greeks = bs.OptionGreeks(*data)
         result = greeks.call_gamma()
-        self.assertAlmostEqual(first=0.01, second=result, delta=0.0002)
+        self.assertEqual(0.01, result)
 
     def test_put_gamma(self):
-        data = (220.0, 200.0, 5.0, 110.0, 2.493)
+        data = (220.0, 200.0, 50.0, 110.0, 2.493)
         greeks = bs.OptionGreeks(*data)
         result = greeks.put_gamma()
-        self.assertAlmostEqual(first=0.0102, second=result, delta=0.0002)
+        self.assertEqual(0.004, result)
 
     def test_call_vega(self):
-        data = (25.0, 30.0, 5.0, 10.0, 2.493)
+        data = (30.0, 30.0, 5.0, 40.0, 2.493)
         greeks = bs.OptionGreeks(*data)
         result = greeks.call_vega()
-        self.assertAlmostEqual(first=0.0, second=result, delta=0.0002)
+        self.assertEqual(0.014, result)
 
     def test_put_vega(self):
         data = (1000.0, 1050.0, 50.0, 110.0, 2.493)
         greeks = bs.OptionGreeks(*data)
         result = greeks.put_vega()
-        self.assertAlmostEqual(first=1.4703, second=result, delta=0.0002)
+        self.assertEqual(1.4703, result)
 
     def test_call_theta(self):
-        data = (25.0, 30.0, 5.0, 10.0, 2.493)
+        data = (29.0, 30.0, 5.0, 10.0, 2.493)
         greeks = bs.OptionGreeks(*data)
         result = greeks.call_theta()
-        self.assertAlmostEqual(first=0.0, second=result, delta=0.0002)
+        self.assertEqual(-0.0002, result)
 
     def test_put_theta(self):
         data = (100.0, 150.0, 350.0, 110.0, 2.493)
         greeks = bs.OptionGreeks(*data)
         result = greeks.put_theta()
-        self.assertAlmostEqual(first=-0.0522, second=result, delta=0.0002)
+        self.assertEqual(-0.0522, result)
 
     def test_call_rho(self):
         data = (25.0, 30.0, 5.0, 10.0, 0.0)
         greeks = bs.OptionGreeks(*data)
         result = greeks.call_rho()
-        self.assertAlmostEqual(first=0.0, second=result, delta=0.001)
+        self.assertEqual(0.0, result)
 
     def test_put_rho(self):
         data = (100.0, 150.0, 350.0, 110.0, 5.0)
         greeks = bs.OptionGreeks(*data)
         result = greeks.put_rho()
-        self.assertAlmostEqual(first=-1.1085, second=result, delta=0.001)
+        self.assertEqual(-1.1078, result)
+
+    def test_invalid_param(self):
+        data = (65.0, 60.0, -30.0, 40.0, 2.493)
+        with self.assertRaises(InvalidDataError):
+            bs.OptionGreeks(*data)
+
+    def test_missing_param(self):
+        data = (65.0, 60.0, 30.0, 25.0)
+        with self.assertRaises(TypeError):
+            bs.OptionGreeks(*data)
 
 
 if __name__ == "__main__":
